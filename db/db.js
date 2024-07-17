a4528bb9d3d93dfe64cba62b76677cdc08656ab7
Sandun Tharuka <sandun.suntws@gmail.com>
2024-07-17 23:01:32 +0530

    initial commit

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+require("dotenv").config();
+
+const uri = process.env.MONGO_DB_URL;
+console.log(uri);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connection established");
+  } catch (error) {
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
